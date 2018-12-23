184d4bdd056e5efba03bc002bd50c51e21b1067e
501260047 <501260047@qq.com>
2018-12-23 13:11:37 +0800

    fistCommit

---
+++ src/pages/userInfo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>姓名：mml</p>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+  p{
+    display: block;
+    background: #77FFFF;
+  }
+</style>
